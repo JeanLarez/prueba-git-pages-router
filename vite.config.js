6713ff23d88216b842531b34e962740d6326e8ee
JeanLarez <jeanlarez@gmail.com>
2024-08-01 13:55:52 -0300

    update src index.html

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    transformReactjsx: true })], 
  base:"https://jeanlarez.github.io/prueba-git-pages-router/"
 })
 
